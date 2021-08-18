9fd33d198596bb54230c9f002581e6c11b279cca
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-08-18 17:42:17 +0600

    overall finish

--- pin.js
+++ pin.js
@@ -1,3 +1,4 @@
+// generate pin
 function getInput() {
   const pin = Math.round(Math.random() * 10000);
   const pinString = pin + '';
@@ -11,4 +12,34 @@ function getInput() {
 function generatePin() {
   const pin = getInput();
   document.getElementById('pin-output').value = pin;
+}
+
+// for calculator
+document.getElementById('keypad').addEventListener('click', function (event) {
+  const number = event.target.innerText;
+  const previousNumberId = document.getElementById('cal-output');
+  if (isNaN(number)) {
+    if (number == 'C') {
+      previousNumberId.value = '';
+    }
+  } else {
+    const previousNumber = previousNumberId.value;
+    const newNumber = previousNumber + number;
+    previousNumberId.value = newNumber;
+  }
+});
+
+// verify pin
+function verifyPin() {
+  const generatePinNum = document.getElementById('pin-output').value;
+  const calInputPinNum = document.getElementById('cal-output').value;
+  const errorMessage = document.getElementById('error');
+  const rightMessage = document.getElementById('right');
+  if (generatePinNum == calInputPinNum) {
+    rightMessage.style.display = 'block';
+    errorMessage.style.display = 'none';
+  } else {
+    rightMessage.style.display = 'none';
+    errorMessage.style.display = 'block';
+  }
 }
